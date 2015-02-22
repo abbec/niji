36b28be91ead69fe0c32a5738ccbbd2b2f0be8eb
Albert Cervin <albert@acervin.com>
2015-02-22 23:34:28 +0100

    A bit more correct tests.

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -5,7 +5,7 @@ import os
 def test_creation(tmpdir):
     repo = Repository("http://localhost:4567", root_dir = str(tmpdir))
 
-    assert os.path.exists(os.path.join(str(tmpdir), repo.cache_dir))
+    assert os.path.exists(repo.cache_dir)
 
 def test_package_file_fetch(tmpdir):
     repo = Repository("http://localhost:4567", root_dir = str(tmpdir))
@@ -13,4 +13,4 @@ def test_package_file_fetch(tmpdir):
     branch = "testbranch"
     repo.get_package_list(branch)
 
-    assert os.path.exists(os.path.join(str(tmpdir), repo.cache_dir, branch))
+    assert os.path.exists(os.path.join(repo.cache_dir, branch))
