7a4efbce80d76e3ff6d99d96d46ce3762364298b
Albert Cervin <albert@acervin.com>
2015-02-22 22:41:20 +0100

    Add repository test.

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -6,3 +6,11 @@ def test_creation(tmpdir):
     repo = Repository("http://localhost:4567", root_dir = str(tmpdir))
 
     assert os.path.exists(os.path.join(str(tmpdir), repo.cache_dir))
+
+def test_package_file_fetch(tmpdir):
+    repo = Repository("http://localhost:4567", root_dir = str(tmpdir))
+
+    branch = "testbranch"
+    repo.get_package_list(branch)
+
+    assert os.path.exists(os.path.join(str(tmpdir), repo.cache_dir, branch))
